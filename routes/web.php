564893664105b573b7666ecda68eb503ded50611
amidou.zabre <amidou.zabre@gmail.com>
2025-08-19 21:34:49 +0000

    Create resource route for Post

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -10,6 +11,17 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('posts', PostController::class)
+        ->except(['show']);
+});
+
+// Route publique pour lecture
+Route::get('/posts/{article:slug}', [PostController::class, 'show'])
+    ->name('posts.show');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
