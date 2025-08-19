e91929e665cf40cc58d4ad60836f74998e41680b
amidou.zabre <amidou.zabre@gmail.com>
2025-08-19 21:09:00 +0000

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title', 'slug', 'excerpt', 'content', 'user_id'
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
