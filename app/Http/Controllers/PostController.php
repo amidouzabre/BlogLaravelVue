e01f250c5ffcb164bd278c1b11fb49463a54d97e
amidou.zabre <amidou.zabre@gmail.com>
2025-08-19 21:18:10 +0000

    PostController->create Show function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -40,7 +40,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //return inertia::render('Articles/Show', ['post' => $post]);
+        return inertia::render('Articles/Show', ['post' => $post]);
     }
 
     /**
